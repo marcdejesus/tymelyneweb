e6ea764bef7f0add2fef104d1d5012d32b56a26a
Marc De Jesus <131229911+marcdejesus@users.noreply.github.com>
2025-02-25 18:59:04 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button"
+
+import './App.css';
+
+export default function Home() {
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
